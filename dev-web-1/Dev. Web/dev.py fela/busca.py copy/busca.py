a811ce2a0c413207178122355b936a1a771ee735
murilow-a16 <muriloantonio903@gmail.com>
2026-09-16 11:47:51 -0300

    Add files via upload

---
+++ dev-web-1/Dev. Web/dev.py fela/busca.py copy/busca.py
@@ -0,0 +1,3 @@
+t = int( input('numero de pessosas'))
+link1 = (t*2)*2
+print(int(link1))
---
+++ dev-web-1/Dev. Web/dev.py fela/esquecida/esquecida.py
@@ -0,0 +1,5 @@
+A = int(input('nota prova'))
+M = int(input('nota media'))
+
+nota2 = (M*2)-A
+print(int(nota2))
